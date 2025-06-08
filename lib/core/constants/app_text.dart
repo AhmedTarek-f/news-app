2b40dc6cae51f8dd4cf769e50c158ee437399244
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-08 14:04:05 +0300

    adding app text

---
+++ lib/core/constants/app_text.dart
@@ -0,0 +1,22 @@
+abstract final class AppText {
+  static const String newsApp = "News App";
+  static const String home = "Home";
+  static const String goodMorning = "Good Morning";
+  static const String someNews = "Here is Some News For You";
+  static const String viewAll = "View All";
+  static const String general = "General";
+  static const String business = "Business";
+  static const String entertainment = "Entertainment";
+  static const String health = "Health";
+  static const String science = "Science";
+  static const String technology = "Technology";
+  static const String sports = "Sports";
+  static const String search = "Search";
+  static const String goToHome = "Go To Home";
+  static const String theme = "Them";
+  static const String dark = "Dark";
+  static const String light = "Light";
+  static const String english = "English";
+  static const String arabic = "Arabic";
+  static const String language = "Language";
+}
