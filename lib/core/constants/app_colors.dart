e4869073d063335d804134869c23496b0c00ced6
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-09 12:45:13 +0300

    adding gray color

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 abstract final class AppColors {
   static const Color black = Color(0xff171717);
   static const Color white = Color(0xffFFFFFF);
+  static const Color gray = Color(0xffA0A0A0);
 }
