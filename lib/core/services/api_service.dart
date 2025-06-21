9e1e00918d35b4b06e9ff8f561f493c0f2c4907d
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-21 17:02:01 +0300

    adding api service

---
+++ lib/core/services/api_service.dart
@@ -0,0 +1,36 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:news_app/core/constants/api_keys.dart';
+import 'package:news_app/core/exceptions/dio_exceptions.dart';
+import 'package:news_app/core/exceptions/failure.dart';
+
+abstract class ApiService {
+  static final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: "https://newsapi.org/v2",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      queryParameters: {"apiKey": ApiKeys.newsApi},
+    ),
+  );
+
+  // GET Request
+  static Future<Either<Failure, Response>> getRequest({
+    required String endpoint,
+    Map<String, dynamic>? queryParams,
+    Map<String, String>? headers,
+  }) async {
+    try {
+      Response response = await _dio.get(
+        endpoint,
+        queryParameters: queryParams,
+        options: Options(headers: headers),
+      );
+      return right(response);
+    } catch (error) {
+      return left(DioExceptions.handleError(error));
+    }
+  }
+}
