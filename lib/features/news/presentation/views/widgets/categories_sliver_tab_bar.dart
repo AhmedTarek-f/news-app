62d0711bf3609c4bbb40515e5000d8055822fd42
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-21 17:03:43 +0300

    adding categories sliver tab bar

---
+++ lib/features/news/presentation/views/widgets/categories_sliver_tab_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:news_app/features/news/presentation/views_model/news_notifier.dart';
+
+class CategoriesSliverTabBar extends ConsumerWidget {
+  const CategoriesSliverTabBar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final String categoryId =
+        ModalRoute.of(context)?.settings.arguments as String;
+    final newsProvider = ref.read(newsNotifierProvider(categoryId).notifier);
+    return SliverAppBar(
+      automaticallyImplyLeading: false,
+      scrolledUnderElevation: 0,
+      pinned: true,
+      toolbarHeight: 30.h,
+      flexibleSpace: FlexibleSpaceBar(
+        background: SafeArea(
+          child: TabBar(
+            isScrollable: true,
+            physics: const BouncingScrollPhysics(),
+            tabAlignment: TabAlignment.start,
+            dividerHeight: 0,
+            unselectedLabelStyle: Theme.of(context).textTheme.bodyMedium,
+            labelStyle: Theme.of(context).textTheme.bodyLarge,
+            indicatorColor: Theme.of(context).colorScheme.inversePrimary,
+            tabs: newsProvider.allCategories
+                .map((category) => Tab(text: category.name))
+                .toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
