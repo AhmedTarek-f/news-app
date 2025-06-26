1e2addd67a2bca6315fe673e7357f600dff198be
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-26 17:12:29 +0300

    adding search route

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:news_app/core/router/route_names.dart';
 import 'package:news_app/features/home/presentation/views/home_view.dart';
 import 'package:news_app/features/news/presentation/views/news_view.dart';
+import 'package:news_app/features/search/presentation/views/search_view.dart';
 
 abstract class AppRouter {
   static Map<String, Widget Function(BuildContext)> routes = {
     RouteNames.home: (_) => const HomeView(),
     RouteNames.news: (_) => const NewsView(),
+    RouteNames.search: (_) => const SearchView(),
   };
 }
--- lib/core/router/route_names.dart
+++ lib/core/router/route_names.dart
@@ -1,4 +1,5 @@
 abstract class RouteNames {
   static const String home = "/HomeView";
   static const String news = "/NewsView";
+  static const String search = "/SearchView";
 }
