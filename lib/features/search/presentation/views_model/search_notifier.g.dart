e870d0c5dc9f1b322e5a3814e155155008891909
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-26 17:20:06 +0300

    adding search notifier

---
+++ lib/features/search/presentation/views_model/search_notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'search_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$searchNotifierHash() => r'1978232f1e761e308a9c40266bb973d55e6cd763';
+
+/// See also [SearchNotifier].
+@ProviderFor(SearchNotifier)
+final searchNotifierProvider =
+    AutoDisposeNotifierProvider<SearchNotifier, SearchState>.internal(
+      SearchNotifier.new,
+      name: r'searchNotifierProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$searchNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$SearchNotifier = AutoDisposeNotifier<SearchState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
