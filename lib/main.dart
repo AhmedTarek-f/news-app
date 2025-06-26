11e00e30cc415637289c8086c7e820f1d55bd31b
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-26 18:47:56 +0300

    updated version

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:news_app/core/di/di.dart';
 import 'package:news_app/news_app.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  configureDependencies();
   FlutterNativeSplash.remove();
   runApp(const ProviderScope(child: NewsApp()));
 }
